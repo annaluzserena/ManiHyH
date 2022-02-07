fcda2c753ac172a921fa73ff3a8287ed63dac58c
annaluzserena <annaluzserena@protonmail.com>
2022-02-07 10:03:45 -0300

    App con componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/app.css';
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import About from "./components/About";
+import Products from "./components/Products";
+import Catalogue from "./components/Catalogue";
+import Download from "./components/Download";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { useRef } from "react";
 
 function App() {
+  const aboutRef = useRef(null);
+  const productsRef = useRef(null);
+  const catalogueRef = useRef(null);
+  const contactRef = useRef(null);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <code>Hello World</code>
-        </p>
+        <Navbar/>
       </header>
+        <Main/>
+        <About/>
+        <Products/>
+        <Catalogue/>
+        <Download/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
