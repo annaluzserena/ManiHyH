a21b8628ab1137f869da018e387122e12670fd9d
Anna Luz Serena <annaluzserena@protonmail.com>
2022-04-20 11:08:08 -0300

    Verificar si es el primer render

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,12 @@ function Navbar() {
         let navlist = document.querySelector(".navlist");
 
         burger.classList.toggle("toggle-burger");
-        navlist.classList.toggle("toggle-navlist");
+        if(!(navlist.classList.contains("toggle-growup")) & !(navlist.classList.contains("toggle-navlist"))) {
+            navlist.classList.add("toggle-navlist");
+        } else {
+            navlist.classList.toggle("toggle-navlist");
         navlist.classList.toggle("toggle-growup");
+        };
     };
 
     const scrollToTop = () => {
@@ -22,36 +26,36 @@ function Navbar() {
     const [navbar, setNavbar] = useState(["white", "rgba(0, 0, 0, 0.70)", "#f4623a", "0 0.5rem 1rem rgb(0 0 0 / 15%)"]);
 
     const listenScrollEvent = e => {
-        if(window.scrollY < 20) {
+        if (window.scrollY < 20) {
             setNavbar(["transparent", "rgba(255, 255, 255, 0.7", "white", "none"]);
             return document.querySelector(".nav").style.boxShadow = navbar[3];
-        } else if(window.scrollY > 20) {
+        } else if (window.scrollY > 20) {
             setNavbar(["white", "rgba(0, 0, 0, 0.70)", "#f4623a", "0 0.5rem 1rem rgb(0 0 0 / 15%)"]);
             return document.querySelector(".nav").style.boxShadow = navbar[3];
         };
     };
 
-        useEffect(() => {
-            if(window.innerWidth >= "1000") {
-                setNavbar(["transparent", "rgba(255, 255, 255, 0.7", "white", "none"]);
-                window.addEventListener("scroll", listenScrollEvent);
-    
+    useEffect(() => {
+        if (window.innerWidth >= "1000") {
+            setNavbar(["transparent", "rgba(255, 255, 255, 0.7", "white", "none"]);
+            window.addEventListener("scroll", listenScrollEvent);
+
             return () =>
-        window.removeEventListener('scroll', listenScrollEvent);
-            }
-        }, []);
+                window.removeEventListener('scroll', listenScrollEvent);
+        }
+    }, []);
 
     return (
         <nav className="nav">
-            <div className="mobile-container" style={{backgroundColor: navbar[0]}}>
-                <a className="scroll-top" onClick={scrollToTop} style={{color: navbar[2]}}>Arriba</a>
+            <div className="mobile-container" style={{ backgroundColor: navbar[0] }}>
+                <a className="scroll-top" onClick={scrollToTop} style={{ color: navbar[2] }}>Arriba</a>
                 <button className="burger-menu" onClick={showMenu}><i className="fas fa-bars"></i></button>
             </div>
-            <ul className="navlist toggle-growup" style={{backgroundColor: navbar[0]}}>
-                <li className="item-1 nav-item" style={{ textDecoration: 'none' }} ><Link to="about" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{color: navbar[1]}} onMouseOver={() => document.querySelectorAll(".nav-link")[0].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[0].style.color = navbar[1]}>Acerca</Link></li>
-                <li className="item-2 nav-item" style={{ textDecoration: 'none' }} ><Link to="products" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{color: navbar[1]}} onMouseOver={() => document.querySelectorAll(".nav-link")[1].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[1].style.color = navbar[1]}>Productos</Link></li>
-                <li className="item-3 nav-item" style={{ textDecoration: 'none' }} ><Link to="catalogue" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{color: navbar[1]}} onMouseOver={() => document.querySelectorAll(".nav-link")[2].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[2].style.color = navbar[1]}>Catálogo</Link></li>
-                <li className="item-4 nav-item" style={{ textDecoration: 'none' }} ><Link to="contact" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{color: navbar[1]}} onMouseOver={() => document.querySelectorAll(".nav-link")[3].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[3].style.color = navbar[1]}>Contáctenos</Link></li>
+            <ul className="navlist" style={{ backgroundColor: navbar[0] }}>
+                <li className="item-1 nav-item" style={{ textDecoration: 'none' }} ><Link to="about" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{ color: navbar[1] }} onMouseOver={() => document.querySelectorAll(".nav-link")[0].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[0].style.color = navbar[1]}>Acerca</Link></li>
+                <li className="item-2 nav-item" style={{ textDecoration: 'none' }} ><Link to="products" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{ color: navbar[1] }} onMouseOver={() => document.querySelectorAll(".nav-link")[1].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[1].style.color = navbar[1]}>Productos</Link></li>
+                <li className="item-3 nav-item" style={{ textDecoration: 'none' }} ><Link to="catalogue" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{ color: navbar[1] }} onMouseOver={() => document.querySelectorAll(".nav-link")[2].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[2].style.color = navbar[1]}>Catálogo</Link></li>
+                <li className="item-4 nav-item" style={{ textDecoration: 'none' }} ><Link to="contact" spy={true} smooth={true} duration={1000} offset={-70} isDynamic={true} onClick={showMenu} className="nav-link" style={{ color: navbar[1] }} onMouseOver={() => document.querySelectorAll(".nav-link")[3].style.color = navbar[2]} onMouseLeave={() => document.querySelectorAll(".nav-link")[3].style.color = navbar[1]}>Contáctenos</Link></li>
             </ul>
         </nav>
     )
